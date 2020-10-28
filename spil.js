09491dbdd43d43c857762bcf3639324f139864dc
Oddenboen <57357627+Oddenboen@users.noreply.github.com>
2020-10-28 11:35:00 +0100

    1. time

---
+++ spil.js
@@ -0,0 +1,18 @@
+var canvasW = 1000
+var canvasH = 500
+var side = 0
+
+
+
+function setup() {
+    createCanvas(canvasW, canvasH);
+    background(153);
+
+  }
+
+
+function draw () {
+    if (side === 2) {
+        background(0);
+     }
+}
