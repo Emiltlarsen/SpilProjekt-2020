ddafffa1728a9907fb775f737459402b74a9dc64
Emiltlarsen <57224623+Emiltlarsen@users.noreply.github.com>
2020-11-06 10:30:55 +0100

    WIP - fiks collision

--- spil.js
+++ spil.js
@@ -1,12 +1,17 @@
 var canvas = document.querySelector('#canvas');
 var context = canvas.getContext('2d');
 
-var xPos = 10;
+var xPos = 200;
 var yPos = 10;
+var prexPos = xPos;
+var preyPos = yPos;
 
-context.rect(xPos, yPos, 50, 50);
+var characterSizeX = 50;
+var characterSizeY = 50;
+
+context.rect(xPos, yPos, characterSizeX, characterSizeY);
+context.rect(100, 100, 10, 10);
 context.stroke();
-
 
 
 function move1(e) {
@@ -14,23 +19,29 @@ function move1(e) {
     //alert(e.KeyCode);
     if(e.keyCode==37) {
         console.log("Venstre pil")
-        xPos -= 5
+        prexPos = xPos + 5;
+        xPos -= 1;
     }
     if(e.keyCode==38) {
         console.log("Op pil")
-        yPos -= 5
+        preyPos = yPos + 5;
+        yPos -= 1;
     }
     if(e.keyCode==39) {
         console.log("Højre pil")
-        xPos += 5
+        prexPos = xPos - 5;
+        xPos += 1;
     }
     if(e.keyCode==40) {
         console.log("Ned pil")
-        yPos += 5
+        preyPos = yPos - 5;
+        yPos += 1;
     }
 
+collision(xPos, yPos);
 canvas.width = canvas.width;
-context.rect(xPos, yPos, 50, 50);
+context.rect(xPos, yPos, characterSizeX, characterSizeY);
+context.rect(100, 100, 50, 50);
 context.stroke();
 
 
@@ -38,3 +49,17 @@ context.stroke();
 
 document.onkeydown = move1
 
+
+function collision(x, y) {
+    if (100 <= xPos >= 150) {
+        xPos = prexPos;
+        console.log("reee")
+    }
+
+    if (100 <= yPos >= 150) {
+        yPos = preyPos;
+        console.log("reeey")
+
+    }
+
+}
