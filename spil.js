d2d0ee9804b90f280b2050a5e9a460848e1d5053
Emiltlarsen <57224623+Emiltlarsen@users.noreply.github.com>
2020-11-12 13:08:01 +0100

    Upcoming collision

--- spil.js
+++ spil.js
@@ -9,8 +9,31 @@ var preyPos = yPos;
 var characterSizeX = 50;
 var characterSizeY = 50;
 
-context.rect(xPos, yPos, characterSizeX, characterSizeY);
-context.rect(100, 100, 10, 10);
+var centerxPos = xPos + (characterSizeX / 2);
+var centeryPos = yPos + (characterSizeY / 2);
+
+character1Pos = {
+    characterSizeX : 50,
+    characterSizeY : 50,
+
+    centerxPos : xPos + (characterSizeX / 2),
+    centeryPos : yPos + (characterSizeY / 2),
+
+    prexPos : centerxPos,
+    preyPos : centeryPos,
+    
+    xPosTL : centerxPos - characterSizeX / 2,    //top left
+    yPosTL : centeryPos - characterSizeY / 2,    //top left
+    xPosTR : centerxPos + characterSizeX / 2,    //top right
+    yPosTR : centeryPos - characterSizeY / 2,    //top right
+    xPosBL : centerxPos - characterSizeX / 2,    //button left
+    yPosBL : centeryPos + characterSizeY / 2,    //button left
+    xPosBR : centerxPos + characterSizeX / 2,    //button right
+    yPosBR : centeryPos + characterSizeY / 2,    //button right
+}
+
+context.rect(character1Pos.xPosTL, character1Pos.yPosTL, character1Pos.characterSizeX, character1Pos.characterSizeY);
+context.rect(100, 100, 100, 10);
 context.stroke();
 
 
@@ -19,30 +42,34 @@ function move1(e) {
     //alert(e.KeyCode);
     if(e.keyCode==37) {
         console.log("Venstre pil")
-        prexPos = xPos + 5;
-        xPos -= 1;
+        character1Pos.prexPos = character1Pos.xPosTL + 5;
+        character1Pos.xPosTL -= 1;
     }
     if(e.keyCode==38) {
         console.log("Op pil")
-        preyPos = yPos + 5;
-        yPos -= 1;
+        character1Pos.preyPos = character1Pos.yPosTL + 5;
+        character1Pos.yPosTL -= 1;
     }
     if(e.keyCode==39) {
         console.log("Højre pil")
-        prexPos = xPos - 5;
-        xPos += 1;
+        character1Pos.prexPos = character1Pos.xPosTL - 5;
+        character1Pos.xPosTL += 1;
     }
     if(e.keyCode==40) {
         console.log("Ned pil")
-        preyPos = yPos - 5;
-        yPos += 1;
+        character1Pos.preyPos = character1Pos.yPosTL - 5;
+        character1Pos.yPosTL += 1;
     }
 
-collision(xPos, yPos);
 canvas.width = canvas.width;
-context.rect(xPos, yPos, characterSizeX, characterSizeY);
-context.rect(100, 100, 50, 50);
+context.rect(character1Pos.xPosTL, character1Pos.yPosTL, character1Pos.characterSizeX, character1Pos.characterSizeY);
+context.rect(100, 100, 100, 10);
 context.stroke();
+collision(character1Pos.xPos, character1Pos.yPos);
+
+
+console.log(character1Pos.xPosTL);
+console.log(character1Pos.yPosTL);
 
 
 }
@@ -50,16 +77,10 @@ context.stroke();
 document.onkeydown = move1
 
 
-function collision(x, y) {
-    if (100 <= xPos >= 150) {
-        xPos = prexPos;
-        console.log("reee")
+function collision() {
+    if (100 <= character1Pos.xPos && character1Pos.xPos <= 200 && 100 <= character1Pos.yPos && character1Pos.yPos <= 110) {
+        character1Pos.xPos = character1Pos.prexPos;
+        character1Pos.yPos = character1Pos.preyPos;
+        console.log("1_______________")
     }
-
-    if (100 <= yPos >= 150) {
-        yPos = preyPos;
-        console.log("reeey")
-
-    }
-
 }
