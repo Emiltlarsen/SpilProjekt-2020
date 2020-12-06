b75b66ac084efe66a65c3833ff49cdb4f4520b15
Emiltlarsen <57224623+Emiltlarsen@users.noreply.github.com>
2020-12-06 02:28:58 +0100

    fix jump og en masse andet, men coll virker

--- spil.js
+++ spil.js
@@ -1,26 +1,29 @@
 //var canvas = document.querySelector('#canvas');
 //var context = canvas.getContext('2d');
 
-var xPos = 200;
-var yPos = 10;
-var prexPos = xPos;
-var preyPos = yPos;
+startPosC1X = 200;
+startPosC1Y = 10;
 
-var characterSizeX = 50;
-var characterSizeY = 50;
+var prexPos = startPosC1X;
+var preyPos = startPosC1Y;
 
-var centerxPos = xPos + (characterSizeX / 2);
-var centeryPos = yPos + (characterSizeY / 2);
+var characterSizeX = 0;
+var characterSizeY = 0;
+
+var centerxPos = startPosC1X + (characterSizeX / 2);
+var centeryPos = startPosC1Y + (characterSizeY / 2);
+
+
 
 character1Pos = {
     characterSizeX : 50,
     characterSizeY : 50,
 
-    centerxPos : xPos + (characterSizeX / 2),
-    centeryPos : yPos + (characterSizeY / 2),
+    centerxPos : startPosC1X + (characterSizeX / 2),
+    centeryPos : startPosC1Y + (characterSizeY / 2),
 
-    prexPos : centerxPos,
-    preyPos : centeryPos,
+//    prexPos : centerxPos,
+//    preyPos : centeryPos,
     
     xPosTL : centerxPos - characterSizeX / 2,    //top left
     yPosTL : centeryPos - characterSizeY / 2,    //top left
@@ -38,20 +41,36 @@ character1Pos = {
 
 
 
+
+
 function setup() {
     createCanvas(500, 500);
     background(153);
     rect(character1Pos.xPosTL, character1Pos.yPosTL, character1Pos.characterSizeX, character1Pos.characterSizeY);
+    rect(100, 100, 300, 10);
 
 }
 
 
 
 function draw() {
+    function jump() {
+        character1Pos.speedY = -10
+        for (let i = 0; i < 2; i++) {
+            character1Pos.speedY =+ i;
+        }
+    }
+
+    function fall() {
+        if (character1Pos.speedY = +1) {
+            character1Pos.speedY = +1
+        }
+
+    }
 
     function move() {
-        character1Pos.xPosTL = character1Pos.xPosTL + character1Pos.speedX
-        character1Pos.yPosTL = character1Pos.yPosTL + character1Pos.speedY
+        character1Pos.xPosTL += character1Pos.speedX
+        character1Pos.yPosTL += character1Pos.speedY
     }
 
     function stopMove() {
@@ -67,6 +86,7 @@ function draw() {
         }
         if(e.keyCode==38) {
             console.log("Op pil")
+            jump()
             //character1Pos.yPosTL -= 1;
         }
         if(e.keyCode==39) {
@@ -80,16 +100,19 @@ function draw() {
         }*/
     }
 
-    collisionButtonY()
+
     move()
+    collisionButtonY()
     stopMove
     background(153);
     rect(character1Pos.xPosTL, character1Pos.yPosTL, character1Pos.characterSizeX, character1Pos.characterSizeY);
+    rect(100, 100, 300, 10);
+    //fall()
 
 
 
 
-    document.onkeydown = move1
+    document.onkeydown = move1;
 
 
 
@@ -101,16 +124,25 @@ function draw() {
         }
     }*/
 
+    //100 <= character1Pos.BL && character1Pos.BL <= 200 && 100 <= character1Pos.BL && character1Pos.BL <= 110 || 100 <= character1Pos.BR && character1Pos.BR <= 200 && 100 <= character1Pos.BR && character1Pos.BR <= 110
+
     function collisionButtonY() {
-        if (100 <= character1Pos.BL && character1Pos.BL <= 200 && 100 <= character1Pos.BL && character1Pos.BL <= 110 || 100 <= character1Pos.BR && character1Pos.BR <= 200 && 100 <= character1Pos.BR && character1Pos.BR <= 110) {
+        if (character1Pos.xPosTL < 100 + 300 &&
+            character1Pos.xPosTL + 50 > 100 &&
+            character1Pos.yPosTL < 100 + 10 &&
+            character1Pos.yPosTL + 50 > 99        
+            ) {
             character1Pos.speedY = 0;
             console.log("1_______________")
-        }
+        } else
+        fall()
     }
+
+
 
     function collisionTopY() {
         if (100 <= character1Pos.TL && character1Pos.TL <= 200 && 100 <= character1Pos.TL && character1Pos.TL <= 110 || 100 <= character1Pos.TR && character1Pos.TR <= 200 && 100 <= character1Pos.TR && character1Pos.TR <= 110) {
-            character1Pos.speedY = -3;
+            character1Pos.speedY = 1;
             console.log("123_____________")
         }
     }
