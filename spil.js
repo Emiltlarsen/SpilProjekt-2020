074377b19140954a15d3c41c46305d76c4ffda35
Oddenboen <57357627+Oddenboen@users.noreply.github.com>
2020-12-09 10:06:34 +0100

    Update spil.js

--- spil.js
+++ spil.js
@@ -10,15 +10,23 @@ function setup() {
 
   }
 
+  function mouseClicked(){
+    if (side === 0 && 200 < mouseX && mouseX < 400 && 300 < mouseY && mouseY < 400){
+      console.log("lmao")
+      
+      side = 1;
+    }
 
+  }
 function draw () {
+  if (side === 1 && 123 < mouseX && mouseX < 246 && 123 < mouseY && mouseY < 246){
+    rect(123,123,123,123)
+  }
 
- if (200 < mouseX && mouseX < 400 && 300 < mouseY && mouseY < 400 ){
-   console.log("lmao")
- }
+  if (side === 2){
+    rect(234,234,234,234)
+  }
 
-    if (side === 2) {
-        background(0);
-     }
-     
+
+   
 }
