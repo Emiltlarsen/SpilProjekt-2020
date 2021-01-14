6fdf114f13eec39a5ad06b07bc6e8357fc05645b
Oddenboen <57357627+Oddenboen@users.noreply.github.com>
2021-01-14 08:42:51 +0100

    Update spil.js

--- spil.js
+++ spil.js
@@ -8,20 +8,28 @@ function setup() {
     createCanvas(canvasW, canvasH);
     //background(153);
     document.body.style.backgroundImage = "url('StartsideP.png')";
+    document.body.style.backgroundRepeat = "no-repeat";
   }
 
 
   function mouseClicked(){
+      //Start game
     if (side === 0 && 625 < mouseX && mouseX < 974 && 428 < mouseY && mouseY < 527){
-      console.log("lmao")
+      console.log("Side 1")
       
       side = 1;
     }
-
+      //Settings
     if (side === 0 && 625 < mouseX && mouseX < 974 && 550 < mouseY && mouseY < 653){
-      console.log("lmao")
+      console.log("Side 2")
       
       side = 2;
+    }
+      //Startside
+    if (side === 2 && 625 < mouseX && mouseX < 974 && 428 < mouseY && mouseY < 527){
+      console.log("Side 0")
+      
+      side = 0;
     }
 
   }
@@ -31,9 +39,11 @@ function draw () {
   }
 
   if (side === 2){
-    document.body.style.backgroundImage = "url('SettingssideP.png')"
+    document.body.style.backgroundImage = "url('SettingssideP.png')";
   }
 
-
+  if (side === 0){
+    document.body.style.backgroundImage = "url('StartsideP.png')";
+  }
    
 }
