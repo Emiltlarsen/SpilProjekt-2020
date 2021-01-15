ab95e8175aac075e9311e0628b9e930e95ac2f95
Oddenboen <57357627+Oddenboen@users.noreply.github.com>
2021-01-15 11:13:31 +0100

    Nye billeder

--- spil.js
+++ spil.js
@@ -4,6 +4,10 @@ var side = 0
 
 
 
+function preload(){
+  img = loadImage('SettingssideP2.png');
+}
+
 function setup() {
     createCanvas(canvasW, canvasH);
     //background(153);
@@ -19,14 +23,14 @@ function setup() {
       
       side = 1;
     }
-      //Settings
+      //Startside til Settings
     if (side === 0 && 625 < mouseX && mouseX < 974 && 550 < mouseY && mouseY < 653){
       console.log("Side 2")
       
       side = 2;
     }
-      //Startside
-    if (side === 2 && 625 < mouseX && mouseX < 974 && 428 < mouseY && mouseY < 527){
+      //Settings til Startside
+    if (side === 2 && 8 < mouseX && mouseX < 128 && 8 < mouseY && mouseY < 59){
       console.log("Side 0")
       
       side = 0;
@@ -34,12 +38,15 @@ function setup() {
 
   }
 function draw () {
-  if (side === 1 && 123 < mouseX && mouseX < 246 && 123 < mouseY && mouseY < 246){
-    rect(123,123,123,123)
+  if (side === 0 && 618 < mouseX && mouseX < 965 && 421 < mouseY && mouseY < 518){
+    fill(0,0,0,1);
+    rect(618,421,347,97);
+  } else {
+    removeElements();
   }
 
   if (side === 2){
-    document.body.style.backgroundImage = "url('SettingssideP.png')";
+    document.body.style.backgroundImage = "url('SettingssideP2.png')";
   }
 
   if (side === 0){
